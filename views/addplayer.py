e5716f1303ff61b01bda09b0b5542bc72af5aab8
rachid ouhammou <rachidouhammou21@gmail.com>
2022-10-21 18:33:03 +0200

    create the first version of the controler

--- views/addplayer.py
+++ views/addplayer.py
@@ -1,6 +1,9 @@
+from . import __View
 
-
-class AddPlayer:
+class AddPlayer(__View):
+    
     count :int = 1
+
     def print(self) -> None:
-        name :str = str(input(f'  what is your name of the player {AddPlayer.count} :\t')
+        name :str = str(input(f'  what is your name of the player {AddPlayer.count} :\t'))
+        
--- views/start.py
+++ views/start.py
@@ -1,6 +1,7 @@
 from utils import clear
+from . import __View
 
-class Start:
+class Start(__View):
 
     """this class represent this view of the strat of the game 
     that show the rule of the game of the XO like this :
@@ -21,7 +22,7 @@ class Start:
         shel we start who will begin first you or your friend !
 
     """
-
+    
     def print(self) -> None:
         
         clear()
